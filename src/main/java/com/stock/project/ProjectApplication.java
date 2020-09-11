bb426853e4f93da637a8a6f70a935df93cf73a55
huobingli <huobingli@outlook.com>
2020-09-11 18:45:49 +0800

    test

---
+++ src/main/java/com/stock/project/ProjectApplication.java
@@ -0,0 +1,15 @@
+package com.stock.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+
+}

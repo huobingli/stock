141b299966f0ac4bae52d8e1dd754fb6a9217edf
huobingli <huobingli@outlook.com>
2020-09-14 19:34:21 +0800

    update

---
+++ src/main/java/com/stock/project/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.stock.project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class LoginController {
+
+    @RequestMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
